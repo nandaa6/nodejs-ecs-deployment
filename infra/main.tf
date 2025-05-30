fa40751692309941b3a213fb005d77ef58c0a080
nandaa6 <bnandakumar29@gmail.com>
2025-05-30 15:59:47 +0530

    updated the ci-cd workflow

--- infra/main.tf
+++ infra/main.tf
@@ -1,17 +1,15 @@
-# terraform {
-#   required_version = ">= 1.3"
-#   backend "s3" {
-#     bucket         = "your-tf-state-bucket"
-#     key            = "fargate/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-locks"
-#   }
+# resource "aws_s3_bucket" "s3_bucket" {
+#   bucket = "terraform-statefile-learnine12" # change this
+# }
 
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
+# resource "aws_dynamodb_table" "terraform_lock" {
+#   name           = "terraform-lock-learnnine12"
+#   billing_mode   = "PAY_PER_REQUEST"
+#   hash_key       = "LockID"
+
+#   attribute {
+#     name = "LockID"
+#     type = "S"
 #   }
 # }
 
