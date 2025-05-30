c25d6123b8554d959ad8eba306bba4564332507a
nandaa6 <bnandakumar29@gmail.com>
2025-05-30 15:10:58 +0530

    updated flow

--- ec2/backend.tf
+++ ec2/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-statefile-learnnine" # change this
+    bucket         = "terraform-statefile-learnine12" # change this
     key            = "statefile/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock-learnnine"
+    dynamodb_table = "terraform-lock-learnnine12"
   }
 }
